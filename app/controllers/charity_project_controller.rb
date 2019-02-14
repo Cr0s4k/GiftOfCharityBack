fd6d5c182d7b2f63a0e4ad13bd14081e6c286efb
Omar Garcia <omarcruz11@hotmail.com>
2019-02-14 14:55:20 +0100

    getCharityProjectInformation with its test

--- app/controllers/charity_project_controller.rb
+++ app/controllers/charity_project_controller.rb
@@ -10,4 +10,20 @@ class CharityProjectController < ApplicationController
     end
     render json: allCharityProjects, status: 200
   end
+
+  def getCharityProjectInformation
+    begin
+      charityProject = CharityProject.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: "Charity project not found" }, status: :not_found
+      return
+    end
+    render json: {
+        id: charityProject.id,
+        name: charityProject.name,
+        price: charityProject.price,
+        imageUrl: charityProject.imageUrl,
+        description: charityProject.description
+    }, status: 200
+  end
 end
