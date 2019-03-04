3a6013774b5bf568f66d890e3d81dede13b5626e
Omar Garcia <omarcruz11@hotmail.com>
2019-03-04 17:36:28 +0100

    No price in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,25 +10,21 @@ CharityProject.destroy_all
 CharityProject.create [
   {
     name: "Save rain forest",
-    price: 10,
     imageUrl: "https://images.pexels.com/photos/904807/pexels-photo-904807.jpeg?cs=srgb&dl=branches-daylight-environment-904807.jpg&fm=jpg",
     description: "Using drones and working with government agencies, two indigenous communities in Peru have gone from losing 5% of their land to 0% deforestation. Indigenous communities are protecting the rainforest even as they face pressure and violence from illegal coca growing, drug trafficking and logging."
   },
   {
     name: "Save the ocean",
-    price: 15,
     imageUrl: "https://img.depresident.com/wp-content/uploads/2016/02/Save-the-Ocean-T-shirts.jpg",
     description: "The Ocean Cleanup develops advanced technologies to rid the world's oceans of plastic. A full-scale deployment of our systems is estimated to clean up 50 % of the Great Pacific Garbage Patch every 5 years."
   },
   {
     name: "Carbon footprint project",
-    price: 10,
     imageUrl: "https://controlequipment.com.au/wp-content/uploads/2016/01/CO2-in-clouds-1170x630.jpg",
     description: "60 MW wind power project in the Indian state of Rajasthan to provide reliable, renewable power to the Rajasthan state electricity grid which is part of the Northern regional electricity grid. The project will lead to reduced greenhouse gas emissions because it displaces electricity from fossil fuel based electricity generation plants.",
   },
   {
     name: "BEF Water Restoration program",
-    price: 35,
     imageUrl: "http://www.baltana.com/files/wallpapers-8/River-Landscape-HD-Wallpapers-25857.jpg",
     description: "Every BEF Water Restoration Certificate® created represents 1000 gallons of water restored on your behalf. By purchasing BEF WRCs® you are directly contributing to the restoration of recreational and ecological vitality in critical freshwater ecosystems."
   }
