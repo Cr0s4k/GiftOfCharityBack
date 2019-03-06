0373fcc0aa742c7c21c7c49c6c957a4cd5c5e5d7
Omar Garcia <omarcruz11@hotmail.com>
2019-03-06 16:38:13 +0100

    No Token fixed

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,7 +2,7 @@ class Gift < ApplicationRecord
   belongs_to :receiver
   belongs_to :video
 
-  after_create do
+  before_create do
     # We create a short random unique token
     # https://web.archive.org/web/20121026000606/http://blog.logeek.fr/2009/7/2/creating-small-unique-tokens-in-ruby
     self.token = rand(36**8).to_s(36)
