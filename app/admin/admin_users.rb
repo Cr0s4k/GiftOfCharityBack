c28b4166b3f367a01b46c58bdefecf196b07a2ec
Omar Garcia <omarcruz11@hotmail.com>
2019-03-19 11:33:58 +0100

    Trackeable info in admin users + some fixes

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -5,24 +5,27 @@ ActiveAdmin.register AdminUser do
     selectable_column
     id_column
     column :email
-    column :current_sign_in_at
+    column :last_sign_in_at
+    column :last_sign_in_ip
     column :sign_in_count
-    column :created_at
     actions
   end
 
   filter :email
-  filter :current_sign_in_at
+  filter :last_sign_in_at
   filter :sign_in_count
   filter :created_at
 
   form do |f|
     f.inputs do
       f.input :email
-      f.input :password
-      f.input :password_confirmation
+      f.input :password unless f.object.new_record?
+      f.input :password_confirmation unless f.object.new_record?
     end
     f.actions
   end
 
+  sidebar :note, only: :new do
+    'An email where the new administrator can set his password will be sent.'
+  end
 end
