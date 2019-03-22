2ec8a87b33848018bd8ce892c179c344403bd55b
Omar Garcia <omarcruz11@hotmail.com>
2019-03-22 12:09:13 +0100

    Gift tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   post '/donations/make_donation', to: 'donations#make_donation'
 
   get '/charity_projects', to: 'charity_projects#get_all_charity_projects'
   get '/charity_projects/:id', to: 'charity_projects#get_charity_project'
 
-  get '/gifts/:token', to: 'gifts#get_gift'
+  resources :gifts, only: ['index'] do
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
