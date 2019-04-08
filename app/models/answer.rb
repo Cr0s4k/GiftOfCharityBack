36e052420a16fe9c404fdb3518649f3a357372d3
Omar Garcia <omarcruz11@hotmail.com>
2019-04-08 16:04:11 +0200

    Admin can crete questionnaires

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :text, presence: true
+
+  scope :correct_ones, -> {where(is_correct: true)}
 end
--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -2,6 +2,7 @@ class Questionnaire < ApplicationRecord
   has_many :questions
   has_many :charity_projects
 
+  validates :title, presence: true
   validates_presence_of :questions
   accepts_nested_attributes_for :questions
 end
