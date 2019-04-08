dec5e9ebd2e231aaa7a0f131b13f9770d7c400e3
Omar Garcia <omarcruz11@hotmail.com>
2019-04-08 13:01:58 +0200

    Can create questionnaires

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :questionnaire
   has_many :answers
+
+  validates_presence_of :answers
+  accepts_nested_attributes_for :answers
 end
--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,4 +1,7 @@
 class Questionnaire < ApplicationRecord
   has_many :questions
   has_many :charity_projects
+
+  validates_presence_of :questions
+  accepts_nested_attributes_for :questions
 end
