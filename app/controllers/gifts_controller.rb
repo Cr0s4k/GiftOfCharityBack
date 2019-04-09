5d494830618ea5491f5ff5402a6fcd5eb4fdf20d
Omar Garcia <omarcruz11@hotmail.com>
2019-04-09 10:52:36 +0200

    Return questionnaire with Gift

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -15,7 +15,28 @@ class GiftsController < ApiController
         videoUrl: gift.video_url,
         donorName: gift.donation.get_donor.name,
         amount: gift.donation.amount,
-        charityProject: gift.donation.charity_project
+        charityProject: gift.donation.charity_project,
+        questionnaire: format_questionnaire(gift.donation.charity_project.questionnaire)
     }, status: :ok
+  end
+
+  private
+  # Format for Quick-quiz
+  def format_questionnaire(questionnaire)
+    {
+        title: questionnaire.title,
+        questions: questionnaire.questions.each_with_index.map do |question, i|
+          {
+              number: i + 1,
+              prompt: question.text,
+              answers: question.answers.map(&:text),
+              correct: {
+                  index: question.answers.each_with_index.find { |answer, _|
+                    answer.is_correct
+                  }[1]
+              }
+          }
+        end
+    }
   end
 end
